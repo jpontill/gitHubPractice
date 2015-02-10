4d82d8cd68b0ad823e78f4b2220ea53b6aadbf63
Courtney Mort <cmort@mines.edu>
2015-02-09 19:49:48 -0700

    Changed mouth height to +30

--- Cat.java
+++ Cat.java
@@ -40,7 +40,7 @@ public class Cat {
 		g2.setColor(Color.red);
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
-		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
+		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT+30);
 		g2.setColor(Color.black);
 		// Meow text appears below cat head, +10 places below 
 		// so it doesn't overlap the drawing
